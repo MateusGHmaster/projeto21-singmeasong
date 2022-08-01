97cd41bd906c902b98818da87b374bbac2bdbeaa
Mateus <mateusoliveira469@gmail.com>
2022-08-01 11:29:36 -0400

    feat: downvote recommendation unique test

--- back-end/test/unitTests/recommendationsUnit.test.ts
+++ back-end/test/unitTests/recommendationsUnit.test.ts
@@ -81,3 +81,41 @@ describe('upvote recommendations', () => {
     });
 
 });
+
+describe('downvote recommendations', () => {
+
+    it('should subtract 1 point of the recommendation score, and delete recommendation if score < -5, given a downvote', async () => {
+
+        const recommendation = {
+
+            id: 1,
+            name: faker.lorem.words(3),
+            youtubeLink: 'https://youtu.be/zW-AIXAnLcE',
+            score: -5
+
+        };
+    
+        jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(recommendation);
+        jest.spyOn(recommendationRepository, 'updateScore').mockResolvedValueOnce({ ...recommendation, score: -6 });
+        jest.spyOn(recommendationRepository, 'remove').mockResolvedValueOnce();
+    
+        await recommendationService.downvote(recommendation.id);
+
+        expect(recommendationRepository.remove).toBeCalledTimes(1);
+
+    });
+  
+    it('should fail to subtract point of the recommendation score, given an inexisting id', async () => {
+
+        jest.spyOn(recommendationRepository, 'find').mockResolvedValueOnce(null);
+    
+        expect(recommendationService.upvote(100)).rejects.toEqual({
+
+            type: 'not_found',
+            message: ''
+
+        });
+
+    });
+
+});
